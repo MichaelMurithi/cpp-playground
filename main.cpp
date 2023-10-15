55bb7770ff547321b7f4d6e2062dabf4211f3a53
Michael Murithi <mykemitrichev@gmail.com>
2023-10-15 22:11:05 +0200

    learn: practiced basic I/O

--- main.cpp
+++ main.cpp
@@ -10,12 +10,27 @@ int addNumbers(int firstNumber, int secondNumber){
     return firstNumber + secondNumber;
 }
 
+void getUserInfo(){
+    int age;
+    std::string name;
+
+    std::cout << "Please type in your last name : " << std::endl;
+    std::cin >> name;
+
+    std::cout << "Please type in your age : " << std::endl;
+    std::cin >> age;
+
+    std::cout << "Hello " << name << "! You are " << age << " years old!" << std::endl;
+}
+
 int main(){
     int firstNumber {12};
     int secondNumber {9};
 
     int sum = addNumbers(firstNumber, secondNumber);
     std::cout << "The sum of the two numbers is : " << sum << std::endl;
+
+    getUserInfo();
 
     return 0;
 }
