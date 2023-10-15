07da1602c4fc6ca125c285cae29c4c93b0f5d032
Michael Murithi <mykemitrichev@gmail.com>
2023-10-15 21:20:02 +0200

    learn: practiced hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    std::cout << "Hello world, " << std::endl;
+    std::cout << "I know it is painful," << std::endl;
+    std::cout << "But I have to learn C++," << std::endl;
+    std::cout << "Yes, it is strange," << std::endl;
+    std::cout << "Yes, I don't feel like," << std::endl;
+    std::cout << "Yes, it is scary," << std::endl;
+    std::cout << "But it is what it is!" << std::endl;
+
+    return 0;
 }
